cb7b81aacd0fecc22e991b1f1d50d97ceafc25be
HackerXeroid <hackerxeroid@outlook.com>
2023-12-03 15:59:50 +0530

    Fix cors error

--- index.js
+++ index.js
@@ -5,19 +5,14 @@ const { Server } = require("socket.io");
 const cors = require("cors");
 
 const app = express();
+
 app.use((req, res, next) => {
-  res.header(
-    "Access-Control-Allow-Origin",
-    "https://chat-app-frontend-k04e.onrender.com/"
-  ); // Change '*' to specific domains if required
+  res.header("Access-Control-Allow-Origin", "*");
   res.header(
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept, Authorization"
   );
-  if (req.method === "OPTIONS") {
-    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, PATCH, DELETE");
-    return res.status(200).json({});
-  }
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, PATCH, DELETE");
   next();
 });
 
